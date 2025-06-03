8d542082f045de98169959c4ffba772fa1f75b3b
oto812 <abashidzeoto@gmail.com>
2025-06-03 14:04:38 +0400

    added httpResponse structure

---
+++ TCPWebServer/HttpResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class HttpResponse
+{
+    public class HttpResponse
+    {
+        public int StatusCode { get; set; }
+        public string StatusText { get; set; }
+        public string ContentType { get; set; }
+        public string Content { get; set; }
+    }
+}
---
+++ TCPWebServer/WebServer.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+public class WebServer
+{
+	public WebServer()
+	{
+	}
+}
