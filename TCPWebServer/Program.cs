6a055e8c7645fe43c3f80f1019a0d899c60a992e
oto812 <abashidzeoto@gmail.com>
2025-06-03 15:03:43 +0400

    implemented program cs to start and handle server

--- TCPWebServer/Program.cs
+++ TCPWebServer/Program.cs
@@ -1,1 +1,40 @@
 ﻿
+using System;
+using System.Threading.Tasks;
+namespace TCPWebServer
+{
+    
+    class Program
+    {
+        private static WebServer _server;
+
+        static async Task Main(string[] args)
+        {
+            
+            Console.CancelKeyPress += (sender, e) =>
+            {
+                e.Cancel = true; 
+                _server?.Stop();
+            };
+
+            try
+            {
+                
+                int port = 8080; 
+                if (args.Length > 0 && int.TryParse(args[0], out int customPort))
+                {
+                    port = customPort;
+                }
+
+                
+                _server = new WebServer(port);
+                await _server.StartAsync(); 
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to start server: {ex.Message}");
+            }
+            Console.WriteLine("Application exiting."); 
+        }
+    }
+}
